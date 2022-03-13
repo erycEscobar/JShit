10a2b55ff85614f4f8751eac8f88b6a6e7cc9b96
Eric Escobar <eryc@live.com.ar>
2022-03-13 11:46:37 -0300

    Correcciones, camelCase y se agrega uso del  metodo Push 13/03

--- JS/main.js
+++ JS/main.js
@@ -1,126 +1,176 @@
-/* --- 02_IncorporarArrays --- */
+/* --- 03_Primera Entrega Del Proyecto Final --- */
 
-const pokedex = [ 
-    {id: 001, pokemon: "Bulbasaur", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 002, pokemon: "Ivysaur", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 003, pokemon: "Venusaur", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 004, pokemon: "Charmander", tipoPrimario: "Fuego", tipoSecundario: "none"},
-    {id: 005, pokemon: "Charmeleon", tipoPrimario: "Fuego", tipoSecundario: "none"},
-    {id: 006, pokemon: "Chariazard", tipoPrimario: "Fuego", tipoSecundario: "Volador"},
-    {id: 007, pokemon: "Squirtle", tipoPrimario: "Agua", tipoSecundario: "none"},
-    {id: 008, pokemon: "Wartortle", tipoPrimario: "Agua", tipoSecundario: "none"},
-    {id: 009, pokemon: "Blastoise", tipoPrimario: "Agua", tipoSecundario: "none"},
-    {id: 010, pokemon: "Caterpie", tipoPrimario: "Bicho", tipoSecundario: "none"},
-    {id: 011, pokemon: "Metapod", tipoPrimario: "Bicho", tipoSecundario: "none"},
-    {id: 012, pokemon: "Buttertfree", tipoPrimario: "Bicho", tipoSecundario: "Volador"},
-    {id: 013, pokemon: "Weedle", tipoPrimario: "Bicho", tipoSecundario: "Veneno"},
-    {id: 014, pokemon: "Kakuna", tipoPrimario: "Bicho", tipoSecundario: "Veneno"},
-    {id: 015, pokemon: "Beedrill", tipoPrimario: "Bicho", tipoSecundario: "Veneno"},
-    {id: 016, pokemon: "Pidgey", tipoPrimario: "Normal", tipoSecundario: "Volador"},
-    {id: 017, pokemon: "Pidgeotto", tipoPrimario: "Normal", tipoSecundario: "Volador"},
-    {id: 018, pokemon: "Pidgeot", tipoPrimario: "Normal", tipoSecundario: "Volador"},
-    {id: 019, pokemon: "Rattata", tipoPrimario: "Normal", tipoSecundario: "None"},
-    {id: 020, pokemon: "Raticate", tipoPrimario: "Normal", tipoSecundario: "None"},
-    {id: 021, pokemon: "Spearow", tipoPrimario: "Normal", tipoSecundario: "Volador"},
-    {id: 022, pokemon: "Fearow", tipoPrimario: "Normal", tipoSecundario: "Volador"},
-    {id: 023, pokemon: "Ekans", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 024, pokemon: "Arbok", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 025, pokemon: "Pikachu", tipoPrimario: "Electrico", tipoSecundario: "None"},
-    {id: 026, pokemon: "Raichu", tipoPrimario: "Electrico", tipoSecundario: "None"},
-    {id: 027, pokemon: "Sandshrew", tipoPrimario: "Tierra", tipoSecundario: "None"},
-    {id: 028, pokemon: "Sandslash", tipoPrimario: "Tierra", tipoSecundario: "None"},
-    {id: 029, pokemon: "NidoranHembra", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 030, pokemon: "Nidorina", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 031, pokemon: "Nidoqueen", tipoPrimario: "Veneno", tipoSecundario: "Tierra"},
-    {id: 032, pokemon: "NidoranMacho", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 033, pokemon: "Nidorino", tipoPrimario: "Veneno", tipoSecundario: "None"},
-    {id: 034, pokemon: "Nidoking", tipoPrimario: "Veneno", tipoSecundario: "Tierra"},
-    {id: 035, pokemon: "Clefairy", tipoPrimario: "Hada", tipoSecundario: "None"},
-    {id: 036, pokemon: "Clefable", tipoPrimario: "Hada", tipoSecundario: "None"},
-    {id: 037, pokemon: "Vulpix", tipoPrimario: "Fuego", tipoSecundario: "None"},
-    {id: 038, pokemon: "Ninetales", tipoPrimario: "Fuego", tipoSecundario: "None"},
-    {id: 039, pokemon: "Jigglypuff", tipoPrimario: "Normal", tipoSecundario: "Hada"},
-    {id: 040, pokemon: "Wigglytuff", tipoPrimario: "Normal", tipoSecundario: "Hada"},
-    {id: 041, pokemon: "Zubat", tipoPrimario: "Veneno", tipoSecundario: "Volador"},
-    {id: 042, pokemon: "Golbat", tipoPrimario: "Veneno", tipoSecundario: "Volador"},
-    {id: 043, pokemon: "Oddish", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 044, pokemon: "Gloom", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 045, pokemon: "Vileplume", tipoPrimario: "Planta", tipoSecundario: "Veneno"},
-    {id: 046, pokemon: "Paras", tipoPrimario: "Bicho", tipoSecundario: "Planta"},
-    {id: 047, pokemon: "Parasect", tipoPrimario: "Bicho", tipoSecundario: "Planta"},
-    {id: 048, pokemon: "Venonat", tipoPrimario: "Bicho", tipoSecundario: "Veneno"},
-    {id: 049, pokemon: "Venomoth", tipoPrimario: "Bicho", tipoSecundario: "Veneno"},
-    {id: 050, pokemon: "Diglett", tipoPrimario: "Tierra", tipoSecundario: "None"},
-    {id: 051, pokemon: "Dugtrio", tipoPrimario: "Tierra", tipoSecundario: "None"},
-    {id: 052, pokemon: "Meowth", tipoPrimario: "Normal", tipoSecundario: "None"},
-    {id: 053, pokemon: "Persian", tipoPrimario: "Normal", tipoSecundario: "None"},
-    {id: 054, pokemon: "Psyduck", tipoPrimario: "Agua", tipoSecundario: "None"},
-    {id: 055, pokemon: "Golduck", tipoPrimario: "Agua", tipoSecundario: "None"},
-    {id: 056, pokemon: "Mankey", tipoPrimario: "Lucha", tipoSecundario: "None"},
-    {id: 057, pokemon: "Primeape", tipoPrimario: "Lucha", tipoSecundario: "None"},
-    {id: 058, pokemon: "Growlithe", tipoPrimario: "Fuego", tipoSecundario: "None"},
-    {id: 059, pokemon: "Arcanine", tipoPrimario: "Fuego", tipoSecundario: "None"},
-    {id: 060, pokemon: "Poliwag", tipoPrimario: "Agua", tipoSecundario: "None"},
-    {id: 061, pokemon: "Poliwhirl", tipoPrimario: "Agua", tipoSecundario: "None"},
-    {id: 062, pokemon: "Poliwrath", tipoPrimario: "Agua", tipoSecundario: "Lucha"},
-    {id: 063, pokemon: "Abra", tipoPrimario: "Psíquico", tipoSecundario: "None"},
-    {id: 064, pokemon: "Kadabra", tipoPrimario: "Psíquico", tipoSecundario: "None"},
-    {id: 065, pokemon: "Alakazam", tipoPrimario: "Psíquico", tipoSecundario: "None"},
-];
+class Pokemon {
 
-let opciones = "12345"
-let nombre = prompt(`Bienvenido al proyecto pokedex, cual es tu nombre?`);
-let opcion = prompt(`Ok ${nombre}, comencemos, esta version de la pokedex cuenta con 65 pokemons, y las opciones son las siguientes: 
+    constructor (id, pokemon, tipoPrimario, tipoSecundario) {
+        this.id = parseInt(id);
+        this.pokemon = pokemon.toUpperCase();
+        this.tipoPrimario = tipoPrimario.toUpperCase();
+        this.tipoSecundario = tipoSecundario.toUpperCase();
+        this.visto = false;
+        this.atrapado = false;
+    }
 
-                     1: Buscar por nombre 
-                     2: Buscar por numero
-                     3: Listar por tipoPrimario 
-                     4: Listar por tipoSecundario 
-                     5: Listarlos a todos
+    pokeVisto() {
+        this.visto = true;
+    }
 
-                     que quieres hacer?`);
-
-function encontrarYlistar (find, propiedad) {
-    for (const pokemon of pokedex) {
-        let searching = pokemon[propiedad];
-        if (searching === find) {
-            console.log(pokemon.id);
-            console.log(pokemon.pokemon);
-            console.log(pokemon.tipoPrimario);
-            console.log(pokemon.tipoSecundario);
-            console.log("---------------------------")
-        }
-    };
+    pokeAtrapado() {
+        this.atrapado = true;
+    }
 }
 
-if (opciones.indexOf(opcion,0) != -1) {
-    switch (opcion) {
+
+
+const arrayPokedex = [ ];
+
+arrayPokedex.push(new Pokemon("1", "Bulbasaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("2", "Ivysaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("3", "Venusaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("4", "Charmander", "Fuego", "none"));
+arrayPokedex.push(new Pokemon("5", "Charmeleon", "Fuego", "none"));
+arrayPokedex.push(new Pokemon("6", "Chariazard", "Fuego", "Volador"));
+arrayPokedex.push(new Pokemon("7", "Squirtle", "Agua", "none"));
+arrayPokedex.push(new Pokemon("8", "Wartortle", "Agua", "none"));
+arrayPokedex.push(new Pokemon("9", "Blastoise", "Agua", "none"));
+arrayPokedex.push(new Pokemon("10", "Caterpie", "Bicho", "none"));
+arrayPokedex.push(new Pokemon("11", "Metapod", "Bicho", "none"));
+arrayPokedex.push(new Pokemon("12", "Buttertfree", "Bicho", "Volador"));
+arrayPokedex.push(new Pokemon("13", "Weedle", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("14", "Kakuna", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("15", "Beedrill", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("16", "Pidgey", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("17", "Pidgeotto", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("18", "Pidgeot", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("19", "Rattata", "Normal", "None"));
+arrayPokedex.push(new Pokemon("20", "Raticate", "Normal", "None"));
+arrayPokedex.push(new Pokemon("21", "Spearow", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("22", "Fearow", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("23", "Ekans", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("24", "Arbok", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("25", "Pikachu", "Electrico", "None"));
+arrayPokedex.push(new Pokemon("26", "Raichu", "Electrico", "None"));
+arrayPokedex.push(new Pokemon("27", "Sandshrew", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("28", "Sandslash", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("29", "NidoranHembra", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("30", "Nidorina", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("31", "Nidoqueen", "Veneno", "Tierra"));
+arrayPokedex.push(new Pokemon("32", "NidoranMacho", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("33", "Nidorino", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("34", "Nidoking", "Veneno", "Tierra"));
+arrayPokedex.push(new Pokemon("35", "Clefairy", "Hada", "None"));
+arrayPokedex.push(new Pokemon("36", "Clefable", "Hada", "None"));
+arrayPokedex.push(new Pokemon("37", "Vulpix", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("38", "Ninetales", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("39", "Jigglypuff", "Normal", "Hada"));
+arrayPokedex.push(new Pokemon("40", "Wigglytuff", "Normal", "Hada"));
+arrayPokedex.push(new Pokemon("41", "Zubat", "Veneno", "Volador"));
+arrayPokedex.push(new Pokemon("42", "Golbat", "Veneno", "Volador"));
+arrayPokedex.push(new Pokemon("43", "Oddish", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("44", "Gloom", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("45", "Vileplume", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("46", "Paras", "Bicho", "Planta"));
+arrayPokedex.push(new Pokemon("47", "Parasect", "Bicho", "Planta"));
+arrayPokedex.push(new Pokemon("48", "Venonat", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("49", "Venomoth", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("50", "Diglett", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("51", "Dugtrio", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("52", "Meowth", "Normal", "None"));
+arrayPokedex.push(new Pokemon("53", "Persian", "Normal", "None"));
+arrayPokedex.push(new Pokemon("54", "Psyduck", "Agua", "None"));
+arrayPokedex.push(new Pokemon("55", "Golduck", "Agua", "None"));
+arrayPokedex.push(new Pokemon("56", "Mankey", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("57", "Primeape", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("58", "Growlithe", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("59", "Arcanine", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("60", "Poliwag", "Agua", "None"));
+arrayPokedex.push(new Pokemon("61", "Poliwhirl", "Agua", "None"));
+arrayPokedex.push(new Pokemon("62", "Poliwrath", "Agua", "Lucha"));
+arrayPokedex.push(new Pokemon("63", "Abra", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("64", "Kadabra", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("65", "Alakazam", "Psíquico", "None"));
+
+
+
+function encontrarListar (find, propiedad) {
+    let arrayTipo = arrayPokedex.filter(pokemon => pokemon[propiedad] === find);
+    for (const pokemon of arrayTipo) {
+        console.log("Nº: " + pokemon.id);
+        console.log("Pokemon: " + pokemon.pokemon);
+        console.log("Tipo Primario: " + pokemon.tipoPrimario);
+        console.log("Tipo Secundario: " + pokemon.tipoSecundario);
+        console.log("---------------------------")
+    }; 
+}
+
+
+
+function nuevoPoke() {
+    let newId = parseInt(prompt("Ingrese nuevo Id"));
+    let newName = prompt("Ingrese nombre del Pokemon");
+    let newTypeP = prompt("Ingrese tipo Primario del nuevo Pokemon");
+    let newTypeS = prompt("Ingrese tipo Secundario del nuevo Pokemon");
+    arrayPokedex.push(new Pokemon(newId, newName, newTypeP, newTypeS));
+    
+    console.log("PokedexActualizada");
+    console.log(arrayPokedex);
+}
+
+
+
+let opciones = "1234567"
+let security = false;
+let nombreUsr = prompt("Bienvenido al proyecto pokedex, cual es tu nombre?");
+let opcionUsr = prompt(`Ok ${nombreUsr}, comencemos, esta version de la pokedex cuenta con ${arrayPokedex.length} pokemons, y las opciones son las siguientes: 
+        
+                        1: Buscar por nombre 
+                        2: Buscar por numero
+                        3: Listar por tipoPrimario 
+                        4: Listar por tipoSecundario 
+                        5: Listarlos a todos
+
+Hola profesor Oak!
+
+                        6: Ingresar nuevo pokemon
+
+                        que quieres hacer?`);
+
+
+
+console.log("PokedexAnterior");
+console.log(arrayPokedex);
+
+
+
+if (opciones.indexOf(opcionUsr,0) != -1) {
+    switch (opcionUsr) {
         case "1":
-            let nombre = prompt(`Ingrese el nombre del pokemon`);
-            encontrarYlistar (nombre, "pokemon");
+            let nombrePoke = prompt("Ingrese el nombre del pokemon");
+            encontrarListar (nombrePoke, "pokemon");
             break;
         case "2":
-            let id = parseInt(prompt(`Ingrese el numero del pokemon`));
-            encontrarYlistar (id, "id");
+            let id = parseInt(prompt("Ingrese el numero del pokemon"));
+            encontrarListar (id, "id");
             break;
         case "3":
-            let tipoPrimario = prompt(`Ingrese el tipoPrimario`);
-            encontrarYlistar (tipoPrimario, "tipoPrimario")
+            let tipoPrimario = prompt("Ingrese el tipoPrimario");
+            encontrarListar (tipoPrimario, "tipoPrimario");
             break;
         case "4":
-            let tipoSecundario = prompt(`Ingrese el tipoSecundario`);
-            encontrarYlistar (tipoSecundario, "tipoSecundario")
+            let tipoSecundario = prompt("Ingrese el tipoSecundario");
+            encontrarListar (tipoSecundario, "tipoSecundario");
             break;
         case "5":
-            for (const pokemon of pokedex) {
+            for (const pokemon of arrayPokedex) {
                 console.log(pokemon.id);
-                console.log(pokemon.pokemon);
-                console.log(pokemon.tipoPrimario);
-                console.log(pokemon.tipoSecundario);
+                console.log("Pokemon: " + pokemon.pokemon);
+                console.log("Tipo Primario: " + pokemon.tipoPrimario);
+                console.log("Tipo Secundario: " + pokemon.tipoSecundario);
                 console.log("---------------------------")
-            };
+            }; 
             break;
-        default:
+        case "6":
+            nuevoPoke();
             break;
     }
 }
