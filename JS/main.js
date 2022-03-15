0495394510778d22a4dba68e46a3d69e116385cf
Eric Escobar <eryc@live.com.ar>
2022-03-14 22:43:53 -0300

    Interactuar con HTML

--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,7 @@ class Pokemon {
         this.tipoSecundario = tipoSecundario.toUpperCase();
         this.visto = false;
         this.atrapado = false;
+        this.img = `./IMG/pokePictures/${id}.png`;
     }
 
     pokeVisto() {
@@ -25,71 +26,108 @@ class Pokemon {
 
 const arrayPokedex = [];
 
-arrayPokedex.push(new Pokemon("1", "Bulbasaur", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("2", "Ivysaur", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("3", "Venusaur", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("4", "Charmander", "Fuego", "none"));
-arrayPokedex.push(new Pokemon("5", "Charmeleon", "Fuego", "none"));
-arrayPokedex.push(new Pokemon("6", "Chariazard", "Fuego", "Volador"));
-arrayPokedex.push(new Pokemon("7", "Squirtle", "Agua", "none"));
-arrayPokedex.push(new Pokemon("8", "Wartortle", "Agua", "none"));
-arrayPokedex.push(new Pokemon("9", "Blastoise", "Agua", "none"));
-arrayPokedex.push(new Pokemon("10", "Caterpie", "Bicho", "none"));
-arrayPokedex.push(new Pokemon("11", "Metapod", "Bicho", "none"));
-arrayPokedex.push(new Pokemon("12", "Buttertfree", "Bicho", "Volador"));
-arrayPokedex.push(new Pokemon("13", "Weedle", "Bicho", "Veneno"));
-arrayPokedex.push(new Pokemon("14", "Kakuna", "Bicho", "Veneno"));
-arrayPokedex.push(new Pokemon("15", "Beedrill", "Bicho", "Veneno"));
-arrayPokedex.push(new Pokemon("16", "Pidgey", "Normal", "Volador"));
-arrayPokedex.push(new Pokemon("17", "Pidgeotto", "Normal", "Volador"));
-arrayPokedex.push(new Pokemon("18", "Pidgeot", "Normal", "Volador"));
-arrayPokedex.push(new Pokemon("19", "Rattata", "Normal", "None"));
-arrayPokedex.push(new Pokemon("20", "Raticate", "Normal", "None"));
-arrayPokedex.push(new Pokemon("21", "Spearow", "Normal", "Volador"));
-arrayPokedex.push(new Pokemon("22", "Fearow", "Normal", "Volador"));
-arrayPokedex.push(new Pokemon("23", "Ekans", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("24", "Arbok", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("25", "Pikachu", "Electrico", "None"));
-arrayPokedex.push(new Pokemon("26", "Raichu", "Electrico", "None"));
-arrayPokedex.push(new Pokemon("27", "Sandshrew", "Tierra", "None"));
-arrayPokedex.push(new Pokemon("28", "Sandslash", "Tierra", "None"));
-arrayPokedex.push(new Pokemon("29", "NidoranHembra", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("30", "Nidorina", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("31", "Nidoqueen", "Veneno", "Tierra"));
-arrayPokedex.push(new Pokemon("32", "NidoranMacho", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("33", "Nidorino", "Veneno", "None"));
-arrayPokedex.push(new Pokemon("34", "Nidoking", "Veneno", "Tierra"));
-arrayPokedex.push(new Pokemon("35", "Clefairy", "Hada", "None"));
-arrayPokedex.push(new Pokemon("36", "Clefable", "Hada", "None"));
-arrayPokedex.push(new Pokemon("37", "Vulpix", "Fuego", "None"));
-arrayPokedex.push(new Pokemon("38", "Ninetales", "Fuego", "None"));
-arrayPokedex.push(new Pokemon("39", "Jigglypuff", "Normal", "Hada"));
-arrayPokedex.push(new Pokemon("40", "Wigglytuff", "Normal", "Hada"));
-arrayPokedex.push(new Pokemon("41", "Zubat", "Veneno", "Volador"));
-arrayPokedex.push(new Pokemon("42", "Golbat", "Veneno", "Volador"));
-arrayPokedex.push(new Pokemon("43", "Oddish", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("44", "Gloom", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("45", "Vileplume", "Planta", "Veneno"));
-arrayPokedex.push(new Pokemon("46", "Paras", "Bicho", "Planta"));
-arrayPokedex.push(new Pokemon("47", "Parasect", "Bicho", "Planta"));
-arrayPokedex.push(new Pokemon("48", "Venonat", "Bicho", "Veneno"));
-arrayPokedex.push(new Pokemon("49", "Venomoth", "Bicho", "Veneno"));
-arrayPokedex.push(new Pokemon("50", "Diglett", "Tierra", "None"));
-arrayPokedex.push(new Pokemon("51", "Dugtrio", "Tierra", "None"));
-arrayPokedex.push(new Pokemon("52", "Meowth", "Normal", "None"));
-arrayPokedex.push(new Pokemon("53", "Persian", "Normal", "None"));
-arrayPokedex.push(new Pokemon("54", "Psyduck", "Agua", "None"));
-arrayPokedex.push(new Pokemon("55", "Golduck", "Agua", "None"));
-arrayPokedex.push(new Pokemon("56", "Mankey", "Lucha", "None"));
-arrayPokedex.push(new Pokemon("57", "Primeape", "Lucha", "None"));
-arrayPokedex.push(new Pokemon("58", "Growlithe", "Fuego", "None"));
-arrayPokedex.push(new Pokemon("59", "Arcanine", "Fuego", "None"));
-arrayPokedex.push(new Pokemon("60", "Poliwag", "Agua", "None"));
-arrayPokedex.push(new Pokemon("61", "Poliwhirl", "Agua", "None"));
-arrayPokedex.push(new Pokemon("62", "Poliwrath", "Agua", "Lucha"));
-arrayPokedex.push(new Pokemon("63", "Abra", "Psíquico", "None"));
-arrayPokedex.push(new Pokemon("64", "Kadabra", "Psíquico", "None"));
-arrayPokedex.push(new Pokemon("65", "Alakazam", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("001", "Bulbasaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("002", "Ivysaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("003", "Venusaur", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("004", "Charmander", "Fuego", "none"));
+arrayPokedex.push(new Pokemon("005", "Charmeleon", "Fuego", "none"));
+arrayPokedex.push(new Pokemon("006", "Chariazard", "Fuego", "Volador"));
+arrayPokedex.push(new Pokemon("007", "Squirtle", "Agua", "none"));
+arrayPokedex.push(new Pokemon("008", "Wartortle", "Agua", "none"));
+arrayPokedex.push(new Pokemon("009", "Blastoise", "Agua", "none"));
+arrayPokedex.push(new Pokemon("010", "Caterpie", "Bicho", "none"));
+arrayPokedex.push(new Pokemon("011", "Metapod", "Bicho", "none"));
+arrayPokedex.push(new Pokemon("012", "Buttertfree", "Bicho", "Volador"));
+arrayPokedex.push(new Pokemon("013", "Weedle", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("014", "Kakuna", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("015", "Beedrill", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("016", "Pidgey", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("017", "Pidgeotto", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("018", "Pidgeot", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("019", "Rattata", "Normal", "None"));
+arrayPokedex.push(new Pokemon("020", "Raticate", "Normal", "None"));
+arrayPokedex.push(new Pokemon("021", "Spearow", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("022", "Fearow", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("023", "Ekans", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("024", "Arbok", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("025", "Pikachu", "Electrico", "None"));
+arrayPokedex.push(new Pokemon("026", "Raichu", "Electrico", "None"));
+arrayPokedex.push(new Pokemon("027", "Sandshrew", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("028", "Sandslash", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("029", "NidoranHembra", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("030", "Nidorina", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("031", "Nidoqueen", "Veneno", "Tierra"));
+arrayPokedex.push(new Pokemon("032", "NidoranMacho", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("033", "Nidorino", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("034", "Nidoking", "Veneno", "Tierra"));
+arrayPokedex.push(new Pokemon("035", "Clefairy", "Hada", "None"));
+arrayPokedex.push(new Pokemon("036", "Clefable", "Hada", "None"));
+arrayPokedex.push(new Pokemon("037", "Vulpix", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("038", "Ninetales", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("039", "Jigglypuff", "Normal", "Hada"));
+arrayPokedex.push(new Pokemon("040", "Wigglytuff", "Normal", "Hada"));
+arrayPokedex.push(new Pokemon("041", "Zubat", "Veneno", "Volador"));
+arrayPokedex.push(new Pokemon("042", "Golbat", "Veneno", "Volador"));
+arrayPokedex.push(new Pokemon("043", "Oddish", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("044", "Gloom", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("045", "Vileplume", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("046", "Paras", "Bicho", "Planta"));
+arrayPokedex.push(new Pokemon("047", "Parasect", "Bicho", "Planta"));
+arrayPokedex.push(new Pokemon("048", "Venonat", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("049", "Venomoth", "Bicho", "Veneno"));
+arrayPokedex.push(new Pokemon("050", "Diglett", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("051", "Dugtrio", "Tierra", "None"));
+arrayPokedex.push(new Pokemon("052", "Meowth", "Normal", "None"));
+arrayPokedex.push(new Pokemon("053", "Persian", "Normal", "None"));
+arrayPokedex.push(new Pokemon("054", "Psyduck", "Agua", "None"));
+arrayPokedex.push(new Pokemon("055", "Golduck", "Agua", "None"));
+arrayPokedex.push(new Pokemon("056", "Mankey", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("057", "Primeape", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("058", "Growlithe", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("059", "Arcanine", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("060", "Poliwag", "Agua", "None"));
+arrayPokedex.push(new Pokemon("061", "Poliwhirl", "Agua", "None"));
+arrayPokedex.push(new Pokemon("062", "Poliwrath", "Agua", "Lucha"));
+arrayPokedex.push(new Pokemon("063", "Abra", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("064", "Kadabra", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("065", "Alakazam", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("066", "Machop", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("067", "Machoke", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("068", "Machamp", "Lucha", "None"));
+arrayPokedex.push(new Pokemon("069", "Bellsprout", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("070", "Weepinbell", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("071", "Victreebel", "Planta", "Veneno"));
+arrayPokedex.push(new Pokemon("072", "Tentacool", "Agua", "Veneno"));
+arrayPokedex.push(new Pokemon("073", "Tentacruel", "Agua", "Veneno"));
+arrayPokedex.push(new Pokemon("074", "Geodude", "Roca", "Tierra"));
+arrayPokedex.push(new Pokemon("075", "Graveler", "Roca", "Tierra"));
+arrayPokedex.push(new Pokemon("076", "Golem", "Roca", "Tierra"));
+arrayPokedex.push(new Pokemon("077", "Ponyta", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("078", "Rapidash", "Fuego", "None"));
+arrayPokedex.push(new Pokemon("079", "Slowpoke", "Agua", "Psiquico"));
+arrayPokedex.push(new Pokemon("080", "Slowbro", "Agua", "Psiquico"));
+arrayPokedex.push(new Pokemon("081", "Magnamite", "Electrico", "Acero"));
+arrayPokedex.push(new Pokemon("082", "Magneton", "Electrico", "Acero"));
+arrayPokedex.push(new Pokemon("083", "Farfetch'd", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("084", "Doduo", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("085", "Dodrio", "Normal", "Volador"));
+arrayPokedex.push(new Pokemon("086", "Seel", "Agua", "None"));
+arrayPokedex.push(new Pokemon("087", "Dewgong", "Agua", "Hielo"));
+arrayPokedex.push(new Pokemon("088", "Grimer", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("089", "Muk", "Veneno", "None"));
+arrayPokedex.push(new Pokemon("090", "Shellder", "Agua", "None"));
+arrayPokedex.push(new Pokemon("091", "Cloyster", "Agua", "Hielo"));
+arrayPokedex.push(new Pokemon("092", "Gastly", "Fantasma", "Veneno"));
+arrayPokedex.push(new Pokemon("093", "Haunter", "Fantasma", "Veneno"));
+arrayPokedex.push(new Pokemon("094", "Gengar", "Fantasma", "Veneno"));
+arrayPokedex.push(new Pokemon("095", "Onix", "Roca", "Tierra"));
+arrayPokedex.push(new Pokemon("096", "Drowzee", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("097", "Hypno", "Psíquico", "None"));
+arrayPokedex.push(new Pokemon("098", "Krabby", "Agua", "None"));
+arrayPokedex.push(new Pokemon("099", "Kingler", "Agua", "None"));
+
+
+
 
 
 
@@ -99,11 +137,22 @@ function encontrarListar (find, propiedad) {
     };
     let arrayTipo = arrayPokedex.filter(pokemon => pokemon[propiedad] === find);
     for (const pokemon of arrayTipo) {
+
         console.log("Nº: " + pokemon.id);
         console.log("Pokemon: " + pokemon.pokemon);
         console.log("Tipo Primario: " + pokemon.tipoPrimario);
         console.log("Tipo Secundario: " + pokemon.tipoSecundario);
         console.log("---------------------------");
+
+        let main = document.getElementById("main");
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<img src=${pokemon.img}></img>
+                                <h3>Nº: ${pokemon.id}</h3>
+                                <p>Nombre: ${pokemon.pokemon} </p>
+                                <p>Tipo Primario: ${pokemon.tipoPrimario}</p>
+                                <p>Tipo Secundario: ${pokemon.tipoSecundario}</p>`;
+        main.appendChild(contenedor);  
+
     }; 
 };
 
@@ -117,10 +166,10 @@ function registrar (find, name, propiedad) {
         let searching = pokemon[name];
         if (searching === find) {
             switch (propiedad) {
-                case "visto":
+                case "VISTO":
                         pokemon.pokeVisto();
                         break;
-                    case "atrapado":
+                    case "ATRAPADO":
                         pokemon.pokeAtrapado();
                         break;
             }; 
@@ -131,6 +180,17 @@ function registrar (find, name, propiedad) {
             console.log("Visto: " + pokemon.visto);
             console.log("Atrapado: " + pokemon.atrapado);
             console.log("---------------------------");
+
+            let main = document.getElementById("main");
+            let contenedor = document.createElement("div");
+            contenedor.innerHTML = `<h2>WOW! HAS ${propiedad} A</h2>
+                                    <img src=${pokemon.img}></img>
+                                    <h3>Nº: ${pokemon.id}</h3>
+                                    <p>Nombre: ${pokemon.pokemon} </p>
+                                    <p>Tipo Primario: ${pokemon.tipoPrimario}</p>
+                                    <p>Tipo Secundario: ${pokemon.tipoSecundario}</p>`;
+            main.appendChild(contenedor);  
+
         };
     };
 };
@@ -144,6 +204,18 @@ function nuevoPoke() {
     let newTypeS = prompt("Ingrese tipo Secundario del nuevo Pokemon");
     arrayPokedex.push(new Pokemon(newId, newName, newTypeP, newTypeS));
     console.log(arrayPokedex);
+
+    for (const pokemon of arrayPokedex) {
+        let main = document.getElementById("main");
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<img src=${pokemon.img}></img>
+                                <h3>Nº: ${pokemon.id}</h3>
+                                <p>Nombre: ${pokemon.pokemon} </p>
+                                <p>Tipo Primario: ${pokemon.tipoPrimario}</p>
+                                <p>Tipo Secundario: ${pokemon.tipoSecundario}</p>`;
+        main.appendChild(contenedor);
+    };
+
 };
 
 
@@ -188,20 +260,31 @@ if (opciones.indexOf(opcionUsr,0) != -1) {
             break;
         case "5":
             for (const pokemon of arrayPokedex) {
-                console.log(pokemon.id);
+
+                console.log("Nº: " + pokemon.id);
                 console.log("Pokemon: " + pokemon.pokemon);
                 console.log("Tipo Primario: " + pokemon.tipoPrimario);
                 console.log("Tipo Secundario: " + pokemon.tipoSecundario);
                 console.log("---------------------------");
-            }; 
+
+                let main = document.getElementById("main");
+                let contenedor = document.createElement("div");
+                contenedor.innerHTML = `<img src=${pokemon.img}></img>
+                                        <h3>Nº: ${pokemon.id}</h3>
+                                        <p>Nombre: ${pokemon.pokemon} </p>
+                                        <p>Tipo Primario: ${pokemon.tipoPrimario}</p>
+                                        <p>Tipo Secundario: ${pokemon.tipoSecundario}</p>`;
+                main.appendChild(contenedor);
+
+            };
             break;
         case "6":
             let pokeVisto = prompt("Ingrese nombre del pokemon");
-            registrar (pokeVisto, "pokemon", "visto");
+            registrar (pokeVisto, "pokemon", "VISTO");
             break;
         case "7":
             let pokeAtrapado = prompt("Ingrese nombre del pokemon");
-            registrar (pokeAtrapado, "pokemon", "atrapado");
+            registrar (pokeAtrapado, "pokemon", "ATRAPADO");
             break;
         case "8":
             nuevoPoke();
